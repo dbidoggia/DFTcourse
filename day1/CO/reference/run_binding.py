d84a17cca3e75514c466873b5eebb6fb1a8fe0e8
Davide Bidoggia <davide.bidoggia@units.it>
2026-02-19 14:23:04 +0100

    ➕ Add references for day1

---
+++ day1/CO/reference/run_binding.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+from ase.io import read
+
+# Create the "binding" directory to store input/output files for different lattice parameters
+os.makedirs("binding", exist_ok=True)
+
+dists = []
+energies = []
+
+# Loop over different lattice parameters, create input files, and run calculations
+for x in np.arange(1., 1.3, 0.03):
+    x = round(x, 2)
+    
+    # Note: f-strings allow embedding variables directly inside curly braces {}
+    print(f"Running calculation for distance = {x} Angstrom", end="\r")
+    
+    # Create an input file for the current lattice parameter
+    # Use f-strings to insert the value of 'x' into the input file content
+    with open(f"binding/co_{x}.in", "w") as f:
+        f.write(f"""
+&CONTROL
+  outdir = './outdirs'
+  prefix = 'co_{x}'
+  pseudo_dir = '../../pseudo'
+/
+&SYSTEM
+  ecutrho =   160
+  ecutwfc =   20
+  ibrav = 0
+  nat = 2
+  ntyp = 2
+/
+&ELECTRONS
+  conv_thr =   1.0d-6
+  electron_maxstep = 80
+  mixing_beta =   0.7
+/
+&IONS
+/
+ATOMIC_SPECIES
+C      12.0107 C.pbe-rrkjus.UPF
+O      15.9994 O.pbe-rrkjus.UPF
+ATOMIC_POSITIONS angstrom
+C   0.00  0.00  0.00
+O   0.00  0.00  {x}  
+K_POINTS gamma
+CELL_PARAMETERS angstrom
+   13.0  0.00  0.00
+   0.00  13.0  0.00
+   0.00  0.00  13.0
+""")
+    # Run the calculation using pw.x (same command used in the terminal)
+    os.system(f"pw.x < binding/co_{x}.in > binding/co_{x}.out")
+
+
+
+    # Read the output file of Quantum ESPRESSO
+    atoms = read(f"binding/co_{x}.out")
+    # Extract the total energy from the output file
+    energy = atoms.get_potential_energy()
+
+    dists.append(x)
+    energies.append(energy)
+    
+# Save the results to a text file for later analysis
+np.savetxt("binding/binding.dat", np.column_stack((dists, energies)), header="Distance (Angstrom)    Energy (eV)")
